81839331aedc7f601a2b41dd8c37862091ef34c7
VedisVigourous <vadanta592007@hotmail.com>
2026-02-14 15:10:01 +0530

    Soln: Day 0 of HackerRank 30DaysofCode

---
+++ HackerRank/30_Days_of_Code/Day_00.java
@@ -0,0 +1,29 @@
+/* 
+ * Day 0: Hello, World.
+ * Link: https://www.hackerrank.com/challenges/30-hello-world
+ * Difficulty: Easy 
+ * Language Used: Java 15
+ * 
+ */
+
+// Solution: 
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello, World.");
+        String var = input.nextLine();
+        System.out.println(var);
+    }
+}
+
+// Explanation: print the required greeting, read one input line, then echo it back.
+
+/* By: Vadanta Kumar Chauhaan
+ * HackerRank: https://www.hackerrank.com/profile/codegrounds666
+ * GitHub: https://github.com/VedisVigourous
+ * Contact vadanta592007@hotmail.com
+ */
