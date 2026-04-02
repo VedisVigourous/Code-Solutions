8dabdabae96ad7621bad2600494d6218a7f87d94
VedisVigourous <vadanta592007@hotmail.com>
2026-04-03 02:25:37 +0530

    Soln: Day 13 of HackerRank 30DaysofCode

---
+++ HackerRank/30_Days_of_Code/Day_13.java
@@ -0,0 +1,50 @@
+/*
+ * Day 12: Inheritance
+ * Link: https://www.hackerrank.com/challenges/30-inheritance
+ * Difficulty: Easy
+ * Language Used: Java 15
+ *
+ */
+
+import java.io.*;
+import java.util.*;
+
+abstract class Book {
+    String title;
+    String author;
+
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    abstract void display();
+}
+
+class MyBook extends Book {
+    int price;
+
+    MyBook(String title , String author , int price){
+        super(title , author);
+        this.price = price;
+    }
+
+    void display(){
+        System.out.printf("Title: %s\n" , title);
+        System.out.printf("Author: %s\n" , author);
+        System.out.printf("Price: %d\n" , price);
+    }
+}
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+
+        Book book = new MyBook(title, author, price);
+        book.display();
+    }
+}
