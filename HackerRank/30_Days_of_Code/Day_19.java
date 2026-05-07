518c10c8cb5c056cc5acc2ac515814bd3cedec75
VedisVigourous <vadanta592007@hotmail.com>
2026-05-07 13:47:33 +0530

    Soln: Day 19 of HackerRank 30DaysofCode

--- HackerRank/30_Days_of_Code/Day_19.java
+++ HackerRank/30_Days_of_Code/Day_19.java
@@ -37,3 +37,18 @@ class Solution {
         System.out.println(sum);
     }
 }
+
+/* -- Explanation: Implement the AdvancedArithmetic interface and sum all divisors of a given number.
+
+   -- Logic used:
+      1. Create a Calculator class that implements AdvancedArithmetic
+      2. Start the divisor sum with 1 and check all numbers up to n
+      3. Add each divisor to the running total
+      4. Print the interface name and the final divisor sum
+*/
+
+/* By: Vadanta Kumar Chauhaan
+ * HackerRank: https://www.hackerrank.com/profile/codegrounds666
+ * GitHub: https://github.com/VedisVigourous
+ * Contact vadanta592007@hotmail.com
+ */
