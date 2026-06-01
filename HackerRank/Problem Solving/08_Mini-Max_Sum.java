70558c356a8b36f8013f4900296ed829ec4fc574
VedisVigourous <vadanta592007@hotmail.com>
2026-06-02 02:57:21 +0530

    Problem Solving 08

--- HackerRank/Problem Solving/08_Mini-Max_Sum.java
+++ HackerRank/Problem Solving/08_Mini-Max_Sum.java
@@ -1,4 +1,4 @@
-/* Problem: Mini-Max Sum
+﻿/* Problem: Mini-Max Sum
    Link: https://www.hackerrank.com/challenges/mini-max-sum
    Difficulty: Easy 
    Language Used: Java8*/
@@ -54,3 +54,12 @@ public class Solution {
         bufferedReader.close();
     }
 }
+
+/*
+ * Author: Vadanta Kumar Chauhaan
+ * HackerRank: https://www.hackerrank.com/profile/codegrounds666
+ * GitHub: https://github.com/VedisVigourous
+ * LinkedIn: https://www.linkedin.com/in/vadanta
+ * Contact: vadanta592007@hotmail.com
+ * Copyright (c) 2026 Vadanta Kumar Chauhaan. All rights reserved.
+ */
