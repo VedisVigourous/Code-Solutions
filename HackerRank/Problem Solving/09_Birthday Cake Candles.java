47d07f3114e59272492f830b3d7b1d0da8984d76
VedisVigourous <vadanta592007@hotmail.com>
2026-06-02 02:57:31 +0530

    Problem Solving 09

---
+++ HackerRank/Problem Solving/09_Birthday Cake Candles.java
@@ -0,0 +1,65 @@
+﻿/* Problem: Mini-Max Sum
+   Link: https://www.hackerrank.com/challenges/birthday-cake-candles
+   Difficulty: Easy 
+   Language Used: Java 15*/
+
+// Solution: 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static int birthdayCakeCandles(List<Integer> candles) {
+        int count = 0;
+        int largestCandle = Integer.MIN_VALUE;
+        
+        for(int elem : candles){
+            if(elem > largestCandle) largestCandle = elem;
+        }
+        
+        for(int elem : candles){
+            if(largestCandle == elem) count++;
+        }
+    
+    return count;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int candlesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> candles = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
+/*
+ * Author: Vadanta Kumar Chauhaan
+ * HackerRank: https://www.hackerrank.com/profile/codegrounds666
+ * GitHub: https://github.com/VedisVigourous
+ * LinkedIn: https://www.linkedin.com/in/vadanta
+ * Contact: vadanta592007@hotmail.com
+ * Copyright (c) 2026 Vadanta Kumar Chauhaan. All rights reserved.
+ */
