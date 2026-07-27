f57af1a38b67d2a32e3caacf8aed2c507f5a70bb
VedisVigourous <vadanta592007@hotmail.com>
2026-07-27 09:43:22 +0530

    Day 1: 10 Days of JavaScript

---
+++ HackerRank/10_Days_of_JavaScript/Day01/02_Functions.js
@@ -0,0 +1,27 @@
+/* 
+ * Day 1: Functions
+ * Link: https://www.hackerrank.com/challenges/js10-function/
+ * Difficulty: Easy 
+ * Language Used: JavaScript
+ * 
+ */
+
+// Solution: 
+function factorial(n){
+    if(n == 0){
+        return 1
+    }
+    else {
+        return n * factorial(n-1)
+    }
+}
+
+
+/*
+ * Author: Vadanta Kumar Chauhaan
+ * HackerRank: https://www.hackerrank.com/profile/codegrounds666
+ * GitHub: https://github.com/VedisVigourous
+ * LinkedIn: https://www.linkedin.com/in/vadanta
+ * Contact: vadanta592007@hotmail.com
+ * Copyright (c) 2026 Vadanta Kumar Chauhaan. All rights reserved.
+ */
