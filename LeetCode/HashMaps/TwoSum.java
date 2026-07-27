2b0d7eab59bd35f762af8af71159d8961ebdfd6e
VedisVigourous <vadanta592007@hotmail.com>
2026-07-27 10:01:32 +0530

    Problem: Two Sum

---
+++ LeetCode/HashMaps/TwoSum.java
@@ -0,0 +1,35 @@
+/* Problem: Two Sum
+   Link: https://leetcode.com/problems/two-sum/
+   Difficulty: Easy 
+   Language Used: Java*/
+
+// Solution:
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        HashMap<Integer , Integer> map = new HashMap<>();
+
+        for(int i = 0 ; i< nums.length ; i++){
+            int required = target - nums[i];
+
+            if(map.containsKey(required)){
+                return new int[] {map.get(required) , i};
+            }
+
+            map.put(nums[i] , i);
+        }
+
+        return new int[]{};
+    }
+}
+
+/*
+ * Author: Vadanta Kumar Chauhaan
+ * HackerRank: https://leetcode.com/u/vedisvigourous/
+ * GitHub: https://github.com/VedisVigourous
+ * LinkedIn: https://www.linkedin.com/in/vadanta
+ * Contact: vadanta592007@hotmail.com
+ * Copyright (c) 2026 Vadanta Kumar Chauhaan. All rights reserved.
+ */
