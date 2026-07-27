fd8abb6c7bba9f9a2bf2a8423ae3f05a1bccce49
VedisVigourous <vadanta592007@hotmail.com>
2026-07-27 09:39:43 +0530

    Day 0: 10 Days of JavaScript

---
+++ HackerRank/10_Days_of_JavaScript/Day00/01_HelloWorld.js
@@ -0,0 +1,26 @@
+/* 
+ * Day 0: Hello, World!.
+ * Link: https://www.hackerrank.com/challenges/js10-hello-world
+ * Difficulty: Easy 
+ * Language Used: JavaScript
+ * 
+ */
+
+// Solution: 
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+
+    // Write a line of code that prints parameterVariable to stdout using console.log:
+    console.log(parameterVariable)
+}
+
+
+/*
+ * Author: Vadanta Kumar Chauhaan
+ * HackerRank: https://www.hackerrank.com/profile/codegrounds666
+ * GitHub: https://github.com/VedisVigourous
+ * LinkedIn: https://www.linkedin.com/in/vadanta
+ * Contact: vadanta592007@hotmail.com
+ * Copyright (c) 2026 Vadanta Kumar Chauhaan. All rights reserved.
+ */
---
+++ HackerRank/10_Days_of_JavaScript/Day00/02_DataTypes.js
@@ -0,0 +1,35 @@
+/* 
+ * Day 0: Data Types
+ * Link: https://www.hackerrank.com/challenges/js10-data-types
+ * Difficulty: Easy 
+ * Language Used: JavaScript
+ * 
+ */
+
+// Solution: 
+function performOperation(secondInteger, secondDecimal, secondString) {
+    
+    const firstInteger = 4;
+    
+    const firstDecimal = 4.0;
+    
+    const firstString = 'HackerRank ';
+
+    const numSecondInteger = Number(secondInteger)
+    console.log(firstInteger + numSecondInteger)
+
+    const convSecondDecimal = Number(secondDecimal)
+    console.log(firstDecimal + convSecondDecimal)
+
+    console.log(firstString + secondString)
+}
+
+
+/*
+ * Author: Vadanta Kumar Chauhaan
+ * HackerRank: https://www.hackerrank.com/profile/codegrounds666
+ * GitHub: https://github.com/VedisVigourous
+ * LinkedIn: https://www.linkedin.com/in/vadanta
+ * Contact: vadanta592007@hotmail.com
+ * Copyright (c) 2026 Vadanta Kumar Chauhaan. All rights reserved.
+ */
