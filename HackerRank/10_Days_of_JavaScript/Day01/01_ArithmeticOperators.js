f5a9ada1981ec92f117d5ab7f5255387a6cdcca7
VedisVigourous <vadanta592007@hotmail.com>
2026-07-27 09:40:02 +0530

    Day 1: 10 Days of JavaScript

---
+++ HackerRank/10_Days_of_JavaScript/Day01/01_ArithmeticOperators.js
@@ -0,0 +1,34 @@
+/* 
+ * Day 1: Arithmetic Operators
+ * Link: https://www.hackerrank.com/challenges/js10-arithmetic-operators
+ * Difficulty: Easy 
+ * Language Used: JavaScript
+ * 
+ */
+
+// Solution: 
+// Function 1
+function getArea(length, width) {
+    let area;
+    // Write your code here
+    area = length * width
+    return area;
+}
+
+// Function 2
+function getPerimeter(length, width) {
+    let perimeter;
+    // Write your code here
+    perimeter = 2*(length+width)
+    return perimeter;
+}
+
+
+/*
+ * Author: Vadanta Kumar Chauhaan
+ * HackerRank: https://www.hackerrank.com/profile/codegrounds666
+ * GitHub: https://github.com/VedisVigourous
+ * LinkedIn: https://www.linkedin.com/in/vadanta
+ * Contact: vadanta592007@hotmail.com
+ * Copyright (c) 2026 Vadanta Kumar Chauhaan. All rights reserved.
+ */
